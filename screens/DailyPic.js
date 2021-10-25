f6f3ba08aba7e3f8a754b098f5ab9eede8125075
pandeykrish088 <76039564+pandeykrish088@users.noreply.github.com>
2021-10-25 14:47:45 +0530

    Add files via upload

---
+++ screens/DailyPic.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+export default class DailyPicScreen extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center"
+                }}>
+            <TouchableOpacity style={{color:'yellow', borderColor:'grey', justifyContent: "left", 
+              }} onPress={() =>{
+                 this.props.navigation.navigate("Home")
+                }}>
+                <Text style={{textAlign:'center', color:"blue", fontWeight:'bold',
+                  fontSize:40, marginLeft:25, marginRight:25}}>  Back </Text>
+                </TouchableOpacity>
+
+                <Text style={{marginTop:30}}>DailyPic Screen!</Text>
+            </View>
+        )
+    }
+}
